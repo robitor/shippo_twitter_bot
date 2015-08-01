4aa7ea9649fd55cb8b1fa57cd217c0f551995430
rohanshah <rohan@goshippo.com>
2015-08-01 14:01:16 -0700

    created webhook, logging response

--- shippo_status_bot.py
+++ shippo_status_bot.py
@@ -1,8 +1,21 @@
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
+# http://stackoverflow.com/questions/20457567/flask-logging-with-foreman
+import logging
+from logging import StreamHandler
+file_handler = StreamHandler()
+app.logger.setLevel(logging.DEBUG)  # set the desired logging level here
+app.logger.addHandler(file_handler)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
+
+
+@app.route('/runscope_webhook', methods=['POST'])
+def runscope_webhook():
+    app.logger.info('Received webhook: %s' % str(request.get_json()))
+    return 'SUCCESS'
